df23d1fe7cd6d2fedb24ba02e63019b9838dcf0b
anushreejois <anushreejois07@gmail.com>
2026-03-26 19:51:10 +0530

    enable cors for frontend deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
